f5ed099e1d0fd41f6c8d4cb7de56bf4bc26b3ec1
Souvik Banerjee <3125253+souviiik@users.noreply.github.com>
2021-07-11 13:36:50 +0530

    Adding user route

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 const express = require("express");
 var cors = require("cors");
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+// Connect Database
+connectDB();
 
+// Init Middleware
+app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send('Welcome to "tripaider api"');
-});
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/profile', require('./routes/api/profile'));
+// app.use('/api/posts', require('./routes/api/posts'));
 
-app.post("/", (req, res) => {
-  res.json({ msg: "This is CORS-enabled for all origins!" });
-});
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-app.listen(PORT, () => {
-  console.log(`Server started on Port ${PORT}`);
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
