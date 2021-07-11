e36a44530059bf3fa8a8b7b8388c0f2a5c872c7a
Souvik Banerjee <3125253+souviiik@users.noreply.github.com>
2021-07-11 22:43:14 +0530

    trail error with CORS

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const path = require("path");
 
 const app = express();
 
-const corsOptions = {
-  origin: "tripaider.com",
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// const corsOptions = {
+//   origin: "tripaider.com",
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
+
 // Connect Database
 connectDB();
 
