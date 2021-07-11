49c56fc88335f268de8e618dfc0343290db85b2c
Souvik Banerjee <3125253+souviiik@users.noreply.github.com>
2021-07-11 13:05:49 +0530

    Adding CORS package

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+var cors = require("cors");
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("Hello from tripaider api");
+  res.send("Welcome to \"tripaider api\"");
 });
 
 app.listen(PORT, () => {
