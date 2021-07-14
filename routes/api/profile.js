bc5bf019e8b451244857bf4b92bdc1d527f2c2dc
Souvik Banerjee <3125253+souviiik@users.noreply.github.com>
2021-07-14 14:38:35 +0530

    enable profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -19,7 +19,7 @@ router.get('/me', auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({
       user: req.user.id
-    }).populate('user', ['fname', 'lname']);
+    }).populate('user');
 
     if (!profile) {
       return res.status(400).json({ msg: 'There is no profile for this user' });
