ccba678ea4e5b7573662fdc2657dacdcfe2d0450
Souvik Banerjee <3125253+souviiik@users.noreply.github.com>
2021-09-07 16:35:47 +0530

    Adding travller profile route

--- routes/api/travellerProfile.js
+++ routes/api/travellerProfile.js
@@ -28,7 +28,7 @@ router.get("/me", auth, async (req, res) => {
     res.json(profile);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send("Server Error", err.message);
   }
 });
 
